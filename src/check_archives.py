2827d6f05682e772ed0108ce638d6a57a1a6136c
tersec <tersec@users.noreply.github.com>
2014-09-16 11:53:24 -0400

    PEP8-ify rest of check_archives.py

--- src/check_archives.py
+++ src/check_archives.py
@@ -10,9 +10,10 @@ def get_file_handlers():
     return dict(chain(*(zip(exts, repeat(fn)) for exts, fn in [
         (['.zip', '.odt', '.ods', '.odp', '.odg', '.docx', '.xlsx', '.pptx',
          '.jar', '.apk', '.cbz', '.epub', '.xpi'],
-         lambda filename:(ZipFile(filename).testzip() == None)),
+         lambda filename:(ZipFile(filename).testzip() is None)),
         (['.xz', '.txz'],
-         lambda filename:(lambda fh:next(filter(lambda _:fh.read(2**24) == b'', repeat(True))))
+         lambda filename:(lambda fh:next(filter(lambda _:fh.read(2**24) == b'',
+                                                repeat(True))))
                          (LZMAFile(filename, 'rb'))),
         (['.png', '.jpg', '.gif', '.tiff', '.tif'],
          lambda filename:bool(Image.open(filename).load()))
@@ -27,11 +28,10 @@ def get_file_handler(filename):
 
 # Until otherwise noted, functions run in multiprocessing
 # subprocesses.
-NUM_UNCHECKED, NUM_CORRECT, NUM_INCORRECT, BYTES_UNCHECKED, \
-BYTES_CORRECT, BYTES_INCORRECT = range(6)
+FILES_GOOD, FILES_BROKEN, DATA_LEFT, DATA_GOOD, DATA_BROKEN = range(5)
 
 
-def check_file_integrity(lock_filename_pair):
+def check_file_intact(lock_filename_pair):
     from os import stat
     _, filename = lock_filename_pair
     handler = get_file_handler(filename)
@@ -40,20 +40,20 @@ def check_file_integrity(lock_filename_pair):
         size = stat(filename).st_size
 
         if not handler:
-            return [1, 0, 0, size, 0, 0]
+            return [0, 0, size, 0, 0]
         if handler(filename):
-            return [0, 1, 0, 0, size, 0]
+            return [1, 0, 0, size, 0]
         else:
-            return [0, 0, 1, 0, 0, size]
+            return [0, 1, 0, 0, size]
 
     except FileNotFoundError:
-        return [0, 0, 0, 0, 0, 0]
+        return [0, 0, 0, 0, 0]
     except PermissionError:
         # stat() shouldn't raise this, but still slightly risky
-        return [1, 0, 0, size, 0, 0]
+        return [0, 0, size, 0, 0]
     except:
         # Otherwise, assume data error (see else branch above)
-        return [0, 0, 1, 0, 0, size]
+        return [0, 1, 0, 0, size]
 
 
 # No display output above here.
@@ -72,26 +72,25 @@ def elide_path(path):
         return path
 
 
-def display_file_integrity(lock_filename_pair):
+def display_file_intact(lock_filename_pair):
     from sys import stdout
     lock, filename = lock_filename_pair
     padding = ' '*(get_available_columns() - len(filename))
 
-    stats = check_file_integrity(lock_filename_pair)
-    assert sorted([stats[_] for _ in [NUM_UNCHECKED, NUM_CORRECT, NUM_INCORRECT]]) \
-           in [[0, 0, 0], [0, 0, 1]]
-    assert sorted([stats[_] for _
-                   in [BYTES_UNCHECKED, BYTES_CORRECT, BYTES_INCORRECT]])[:2] == [0, 0]
+    stats = check_file_intact(lock_filename_pair)
+    assert sorted(map(lambda _: stats[_],
+                      [DATA_LEFT, DATA_GOOD, DATA_BROKEN]))[:2] == [0, 0]
 
     lock.acquire()
-    if stats[NUM_CORRECT] == 1:
+    if stats[FILES_GOOD] == 1:
         stdout.write('\r%s: ok  %s' % (elide_path(filename), padding))
-    elif stats[NUM_INCORRECT] == 1:
+    elif stats[FILES_BROKEN] == 1:
         # Need to pad to wipe out rest of previous 'ok' lines
         stdout.write('\r%s: fail%s\n' % (filename, padding))
     lock.release()
 
-    # multiprocessing pools only functions with top-level functions because those can be pickled
+    # multiprocessing pools only functions with top-level functions
+    # because those can be pickled
     return stats
 
 
@@ -110,21 +109,19 @@ def check_files(root, check_fn):
     from multiprocessing import Manager, Pool, cpu_count
 
     with Manager() as m:
-        stats = Pool(processes=cpu_count()).map(check_fn, zip(repeat(m.Lock()), search_dir(root)))
+        stats = Pool(cpu_count()).map(check_fn,
+                                      zip(repeat(m.Lock()), search_dir(root)))
 
-    total = reduce(lambda a, b: [x+y for x, y in zip(a, b)], stats, [0, 0, 0, 0, 0, 0])
-    return \
-        total[NUM_CORRECT]+total[NUM_INCORRECT]+total[NUM_UNCHECKED], \
-        total[NUM_CORRECT]+total[NUM_INCORRECT], total[NUM_CORRECT], \
-        total[BYTES_CORRECT]+total[BYTES_INCORRECT]+total[BYTES_UNCHECKED], \
-        total[BYTES_CORRECT]+total[BYTES_INCORRECT], total[BYTES_CORRECT]
+    return reduce(lambda a, b: [x+y for x, y in zip(a, b)],
+                  stats, [0, 0, 0, 0, 0, 0])
 
 
 def cmdline_parser():
     from optparse import OptionParser
-    parser = OptionParser(usage='usage %prog [options] [scan_directory]', version="%prog 0.5")
-    # parser.add_option('-v', action='store_true', dest='verbose', help='display progress')
-    parser.add_option('-q', action='store_false', dest='verbose', help='produce no output', default=True)
+    parser = OptionParser(usage='usage %prog [options] [scan_directory]',
+                          version="%prog 0.5")
+    parser.add_option('-q', action='store_false', dest='verbose',
+                      help='produce no output', default=True)
 
     (options, args) = parser.parse_args()
     return options.verbose, args
@@ -133,19 +130,22 @@ def cmdline_parser():
 def main():
     from sys import stdout
     verbose, args = cmdline_parser()
-    t, s, c, t_bytes, s_bytes, c_bytes = check_files(args[0] if args != [] else '.',
-                                                     display_file_integrity if verbose else check_file_integrity)
+    info = check_files(args[0] if args != [] else '.',
+                       display_file_intact if verbose else check_file_intact)
+
     if verbose:
-        # TODO: this message lies (can get here even w/ failures)
-        # avoid division by zero.
-        stdout.write('\r%s\n' % ('all ok; tested %2.2f%% (%d of %d) of available files and %2.2f%% of available data'
-                       % (s*100.0/(t+1), s, t, s_bytes*100.0/(t_bytes+1)) if s == c else
-                       'failed%s' % (' '*(get_available_columns()-6))))
-    return not (s == c)
+        stdout.write(
+            '\r%s\n' % ('all ok; tested %2.2f%% of available data'
+                        % (info[DATA_GOOD]*100 /
+                           (info[DATA_LEFT]+info[DATA_GOOD]+1))
+                        if info[FILES_BROKEN] == 0
+                        else 'failed%s' % (' '*(get_available_columns()-6))))
+    return info[FILES_BROKEN] != 0
 
 if __name__ == '__main__':
     # For multiprocessing on Win32
     from multiprocessing import freeze_support
     freeze_support()
 
-    from sys import exit; exit(main())
+    from sys import exit
+    exit(main())
